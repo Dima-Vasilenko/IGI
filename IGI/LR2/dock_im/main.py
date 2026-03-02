383079da8b6e3e1e417235159d9c58a49b16be60
dima <dmitriacwor@gmail.com>
2026-03-02 19:52:38 +0300

    Lab worl with docker

---
+++ IGI/LR2/dock_im/main.py
@@ -0,0 +1,9 @@
+import geometric_lib.square as square
+import geometric_lib.circle as circle
+import os
+
+radius = float(os.environ.get("R", 0))
+a = float(os.environ.get("A", 0))
+
+print("Square area: ", circle.area(radius))
+print("Rectangle area:", square.area(a))
